9591b796d068d0143d1a8030e8d196ee465ace98
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-03-09 17:25:17 +0530

    finished hero component in myPostFolio

--- 01_myPortFolio/src/components/Hero/Hero.jsx
+++ 01_myPortFolio/src/components/Hero/Hero.jsx
@@ -2,54 +2,54 @@ import React from "react";
 import heroImage from "../../assets/hero-img.png";
 import sunImage from "../../assets/sun.svg";
 import moonImage from "../../assets/moon.svg";
-import styles from "./HeroStyles.module.css"
-import instaLight from '../../assets/instagram-light.svg';
-import instaDark from '../../assets/instagram-dark.svg';
-import githubLight from '../../assets/github-light.svg';
-import githubDark from '../../assets/github-dark.svg';
-import linkedinLight from '../../assets/linkedin-light.svg';
-import linkedinDark from '../../assets/linkedin-dark.svg';
-import {useTheme} from "../../common/ThemeContext.jsx"
+import styles from "./HeroStyles.module.css";
+import instaLight from "../../assets/instagram-light.svg";
+import instaDark from "../../assets/instagram-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import { useTheme } from "../../common/ThemeContext.jsx";
 
 function Hero() {
-  const {theme,toggleTheme} = useTheme();
-  const themeImg = theme==="light"?sunImage:moonImage;
-  const githubImg = theme==="light"?githubLight:githubDark;
-  const linkedinImg = theme==="light"?linkedinLight:linkedinDark;
-  const instaImg = theme==="light"?instaLight:instaDark;
+  const { theme, toggleTheme } = useTheme();
+  const themeImg = theme === "light" ? sunImage : moonImage;
+  const githubImg = theme === "light" ? githubLight : githubDark;
+  const linkedinImg = theme === "light" ? linkedinLight : linkedinDark;
+  const instaImg = theme === "light" ? instaLight : instaDark;
 
   return (
     <>
       <section id="hero" className={styles.container}>
         <div className={styles.heroContainer}>
-          <img src={heroImage} alt="profile-image"  />
-          <img src={themeImg} alt="theme-image" onClick={toggleTheme}/>
+          <img src={heroImage} alt="profile-image" className={styles.hero} />
+          <img src={themeImg} alt="theme-image" onClick={toggleTheme} className={styles.colorMode}/>
         </div>
         <div className={styles.info}>
           <h1>
-            Sudeep 
-            <br /> 
-            Bhandari 
+            Sudeep
+            <br />
+            Bhandari
           </h1>
-          <h2>Full Stack Developer</h2>
-          <span >
-          <a href="https://instagram.com/" target="_blank">
-            <img src={instaImg} alt="Twitter icon" />
+          <h2>Fullstack Developer</h2>
+          <span className={styles.linkContainer}>
+            <a href="https://www.instagram.com/soo__deeep?igsh=MWNqZDRvZmd0OW1obw==" target="_blank">
+              <img src={instaImg} alt="Twitter icon" />
+            </a>
+            <a href="https://github.com/SudeepBhandari02" target="_blank">
+              <img src={githubImg} alt="Github icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/sudeepbhandari02" target="_blank">
+              <img src={linkedinImg} alt="Linkedin icon" />
+            </a>
+          </span>
+          <p className={styles.description}>
+            With a passion for developing modern React web apps for commercial
+            businesses.
+          </p>
+          <a className="resumeLink" href="#" download>
+            <button className="hover">Resume</button>
           </a>
-          <a href="https://github.com/" target="_blank">
-            <img src={githubImg} alt="Github icon" />
-          </a>
-          <a href="https://linkedin.com/" target="_blank">
-            <img src={linkedinImg} alt="Linkedin icon" />
-          </a>
-        </span>
-        <p className={styles.description}>
-          With a passion for developing modern React web apps for commercial
-          businesses.
-        </p>
-        <a href="#" download>
-          <button className="hover">Resume</button>
-        </a>
         </div>
       </section>
     </>
